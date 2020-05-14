b0a6ff3f1f4b38e64214f667d2df2443e60060e9
5enxia <c011729632@edu.teu.ac.jp>
2020-05-14 21:35:01 +0900

    Add prototype single_multi_gpu.py

--- proto/single_multi_gpu.py
+++ proto/single_multi_gpu.py
@@ -1,10 +1,11 @@
 import sys
-import cupy as cp
 import numpy as np
+import six
+import fastrlock
+import cupy as cp
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-with cp.cuda.Device(rank):
-    print(f'Device ID: {rank}')
+print(f'Device ID: {rank}')
