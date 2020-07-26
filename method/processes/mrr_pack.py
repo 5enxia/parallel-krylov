003778e2dc53152198a115a8eef35047374426f5
takayasumorishita <c011729632@edu.teu.ac.jp>
2020-07-26 10:09:28 +0900

    Remove y scatter & Remove local_Ar

--- method/processes/mrr_pack.py
+++ method/processes/mrr_pack.py
@@ -35,13 +35,10 @@ def _mrr_gpu(A, b, epsilon, T, pu):
     # 初期反復
     if rank == 0:
         start_time = start(method_name='MrR')
-    # local_Ar = A[begin:end].dot(r)
-    # comm.Allgather(local_Ar.get(), Ar_cpu)
-    Ar[begin:end] = A[begin:end].dot(r)
-    comm.Allgather(Ar[begin:end].get(), Ar_cpu)
+    # Ar[begin:end] = A[begin:end].dot(r)
+    # comm.Allgather(Ar[begin:end].get(), Ar_cpu)
+    comm.Allgather(A[begin:end].dot(r).get(), Ar_cpu)
     Ar = cp.asarray(Ar_cpu)
-    # rsss[0] = r[begin:end].dot(local_Ar)
-    # rsss[1] = local_Ar.dot(local_Ar)
     rsss[0] = r[begin:end].dot(Ar[begin:end])
     rsss[1] = Ar[begin:end].dot(Ar[begin:end])
     comm.Allreduce(rsss.get(), rsss_cpu)
@@ -63,14 +60,12 @@ def _mrr_gpu(A, b, epsilon, T, pu):
             break
 
         # 解の更新
-        # local_Ar = A[begin:end].dot(r)
-        # comm.Allgather(local_Ar.get(), Ar_cpu)
-        Ar[begin:end] = A[begin:end].dot(r)
-        comm.Allgather(Ar[begin:end].get(), Ar_cpu)
+        # Ar[begin:end] = A[begin:end].dot(r)
+        # comm.Allgather(Ar[begin:end].get(), Ar_cpu)
+        comm.Allgather(A[begin:end].dot(r).get(), Ar_cpu)
         Ar = cp.asarray(Ar_cpu)
-        comm.Scatter(y.get(), y_cpu[begin:end])
-        y[begin:end] = cp.asarray(y_cpu[begin:end])
-        # numu[0] = y[begin:end].dot(local_Ar)
+        # comm.Scatter(y.get(), y_cpu[begin:end])
+        # y[begin:end] = cp.asarray(y_cpu[begin:end])
         numu[0] = y[begin:end].dot(Ar[begin:end])
         numu[1] = y[begin:end].dot(y[begin:end])
         comm.Allreduce(numu.get(), numu_cpu)
