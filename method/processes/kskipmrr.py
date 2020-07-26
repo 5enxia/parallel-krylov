0afee41af056bd5e8eda056bfefcb99bd0125e6e
takayasumorishita <c011729632@edu.teu.ac.jp>
2020-07-27 05:11:50 +0900

    Refactor

--- method/processes/kskipmrr.py
+++ method/processes/kskipmrr.py
@@ -157,8 +157,6 @@ def _kskipmrr_gpu(A, b, epsilon, k, T, pu):
     Ax = np.empty(N, T)
     Ar = cp.zeros((k + 2, N), T)
     Ay = cp.zeros((k + 1, N), T)
-    # Ar = cp.zeros((k + 3 + 1, N), T)
-    # Ay = cp.zeros((k + 2 + 1, N), T)
     rAr = cp.empty(1, T)
     ArAr = cp.empty(1, T)
     alpha = cp.zeros(2*k + 3, T)
@@ -167,8 +165,6 @@ def _kskipmrr_gpu(A, b, epsilon, k, T, pu):
     # cpu
     Ar_cpu = np.zeros((k + 2, N), T)
     Ay_cpu = np.zeros((k + 1, N), T)
-    # Ar_cpu = np.zeros((k + 3 + 1, N), T)
-    # Ay_cpu = np.zeros((k + 2 + 1, N), T)
     rAr_cpu = np.empty(1, T)
     ArAr_cpu = np.empty(1, T)
     alpha_cpu = np.zeros(2*k + 3, T)
@@ -207,30 +203,26 @@ def _kskipmrr_gpu(A, b, epsilon, k, T, pu):
         if isConverged:
             break
 
-        # 事前計算
-        # for j in range(1, (k + 2) + 1, 2):
-        for j in range(1, k + 2):
-            # local_Ar[0][begin:end] = A[begin:end].dot(Ar[j-1])
-            # local_Ar[1] = A[begin:end].T.dot(local_Ar[0][begin:end])
-            # comm.Reduce(local_Ar.get(), Ar_cpu[j:j+2])
-            comm.Allgather(A[begin:end].dot(Ar[j-1]).get(), Ar_cpu[j])
-            Ar[j] = cp.asarray(Ar_cpu[j])
-        # for j in range(1, (k + 1) + 1, 2):
+        # 基底計算
         for j in range(1, k + 1):
-            # local_Ay[0][begin:end] = A[begin:end].dot(Ay[j-1])
-            # local_Ay[1] = A[begin:end].T.dot(local_Ay[0][begin:end])
-            # comm.Reduce(local_Ay.get(), Ay_cpu[j:j+2])
+            comm.Allgather(A[begin:end].dot(Ar[j-1]).get(), Ar_cpu[j])
             comm.Allgather(A[begin:end].dot(Ay[j-1]).get(), Ay_cpu[j])
+            Ar[j] = cp.asarray(Ar_cpu[j])
             Ay[j] = cp.asarray(Ay_cpu[j])
-        for j in range(2*k + 3):
+        comm.Allgather(A[begin:end].dot(Ar[k]).get(), Ar_cpu[k+1])
+        Ar[k+1] = cp.asarray(Ar_cpu[k+1])
+        
+        # 係数計算
+        alpha[0] = Ar[0][begin:end].dot(Ar[0][begin:end])
+        delta[0] = Ay[0][begin:end].dot(Ay[0][begin:end])
+        for j in range(1, 2*k+1):
             jj = j//2
             alpha[j] = Ar[jj][begin:end].dot(Ar[jj + j % 2][begin:end])
-        for j in range(1, 2*k + 2):
-            jj = j//2
             beta[j] = Ay[jj][begin:end].dot(Ar[jj + j % 2][begin:end])
-        for j in range(2*k + 1):
-            jj = j//2
             delta[j] = Ay[jj][begin:end].dot(Ay[jj + j % 2][begin:end])
+        alpha[2*k+1] = Ar[k][begin:end].dot(Ar[k+1][begin:end])
+        beta[2*k+1] = Ay[k][begin:end].dot(Ar[k+1][begin:end])
+        alpha[2*k+2] = Ar[k+1][begin:end].dot(Ar[k+1][begin:end])
         comm.Allreduce(alpha.get(), alpha_cpu)
         comm.Allreduce(beta.get(), beta_cpu)
         comm.Allreduce(delta.get(), delta_cpu)
@@ -245,19 +237,20 @@ def _kskipmrr_gpu(A, b, epsilon, k, T, pu):
         Ay[0] = eta * Ay[0] + zeta * Ar[1]
         z = eta * z - zeta * Ar[0]
         Ar[0] -= Ay[0]
-        comm.Allgather(A[begin:end].dot(Ar[0]).get(), Ar_cpu[1])
-        Ar[1] = cp.asarray(Ar_cpu[1])
         x -= z
 
         # MrRでのk反復
         for j in range(k):
-            delta[0] = zeta ** 2 * alpha[2] + eta * zeta * beta[1]
+            zz = zeta ** 2
+            ee = eta ** 2
+            ez = eta * zeta
+            delta[0] = zz * alpha[2] + ez * beta[1]
             alpha[0] -= zeta * alpha[1]
-            delta[1] = eta ** 2 * delta[1] + 2 * eta * zeta * beta[2] + zeta ** 2 * alpha[3]
+            delta[1] = ee * delta[1] + 2 * eta * zeta * beta[2] + zz * alpha[3]
             beta[1] = eta * beta[1] + zeta * alpha[2] - delta[1]
             alpha[1] = -beta[1]
             for l in range(2, 2 * (k - j) + 1):
-                delta[l] = eta ** 2 * delta[l] + 2 * eta * zeta * beta[l+1] + zeta ** 2 * alpha[l + 2]
+                delta[l] = ee * delta[l] + 2 * ez * beta[l+1] + zz * alpha[l + 2]
                 tau = eta * beta[l] + zeta * alpha[l + 1]
                 beta[l] = tau - delta[l]
                 alpha[l] -= tau + beta[l]
@@ -265,11 +258,11 @@ def _kskipmrr_gpu(A, b, epsilon, k, T, pu):
             d = alpha[2] * delta[0] - beta[1] ** 2
             zeta = alpha[1] * delta[0] / d
             eta = -alpha[1] * beta[1] / d
+            comm.Allgather(A[begin:end].dot(Ar[0]).get(), Ar_cpu[1])
+            Ar[1] = cp.asarray(Ar_cpu[1])
             Ay[0] = eta * Ay[0] + zeta * Ar[1]
             z = eta * z - zeta * Ar[0]
             Ar[0] -= Ay[0]
-            comm.Allgather(A[begin:end].dot(Ar[0]).get(), Ar_cpu[1])
-            Ar[1] = cp.asarray(Ar_cpu[1])
             x -= z
 
         i += (k + 1)
