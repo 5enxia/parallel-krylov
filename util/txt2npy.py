0d4cecdbf8ddd410a171bc43181b2d4c0eff63e6
5enxia <c011729632@edu.teu.ac.jp>
2020-11-09 15:33:42 +0900

    Change outputfile option

--- util/txt2npy.py
+++ util/txt2npy.py
@@ -1,6 +1,10 @@
 import loader
 import argparse
 import numpy as np
+<<<<<<< Updated upstream
+=======
+import os
+>>>>>>> Stashed changes
 
 parser = argparse.ArgumentParser(description='.txt to .npy converter')
 parser.add_argument("path", help="data file path")
@@ -11,11 +15,13 @@ parser.add_argument("-v", "--vector", action="store_true",
                     help="vector file")
 
 args = parser.parse_args()
+path, basename = os.path.split(args.path)
+filename, ext = os.path.splitext(basename)
 if args.matrix:
     matrix = loader.load_matrix(args.path, args.type)
-    np.save('matrix.npy', matrix)
+    np.save(f'{path}/{filename}.npy', matrix)
 elif args.vector:
     vector = loader.load_vector(args.path, args.type)
-    np.save('vector.npy', vector)
+    np.save(f'{path}/{filename}.npy', vector)
 
 print(args)
