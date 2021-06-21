add32e804deefa378a58f5a7ed015f7b47efca66
takayasu morishita <c011729632@edu.teu.ac.jp>
2021-06-21 18:40:48 +0900

    Update mtx2npz

--- util/converter/mtx2npz.py
+++ util/converter/mtx2npz.py
@@ -1,14 +1,14 @@
 import os
 import argparse
-from scipy.io import mmread as read
-from scipy import sparse
+from scipy.io import mmread
+from scipy.sparse import save_npz
 
-parser = argparse.ArgumentParser(description='.txt to .npy converter')
+parser = argparse.ArgumentParser(description='.mtx to .npz converter')
 parser.add_argument("path", help="data file path")
 
 args = parser.parse_args()
 path, basename = os.path.split(args.path)
 filename, ext = os.path.splitext(basename)
 
-a = read(args.path)
-sparse.save(f'{path}/{basename}/.npz', a)
+mtx = mmread(args.path)
+save_npz(f'{path}/{basename}/.npz', mtx)
