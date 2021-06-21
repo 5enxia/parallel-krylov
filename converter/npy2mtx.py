f70d919b7807c2aef957a36a74ed9139469adc38
takayasu morishita <c011729632@edu.teu.ac.jp>
2021-06-21 15:29:17 +0900

    Add converter npy2npz,mtx

---
+++ converter/npy2mtx.py
@@ -0,0 +1,28 @@
+import argparse
+import numpy as np
+import os
+
+from scipy.sparse import csr_matrix
+from scipy.io import mmwrite, mmread 
+
+# help
+parser = argparse.ArgumentParser(description='.npy to .mtx(csr) converter')
+parser.add_argument("path", help="data file path")
+
+# parse argv
+args = parser.parse_args()
+path, basename = os.path.split(args.path)
+filename, ext = os.path.splitext(basename)
+
+# load npy
+npy = np.load(f'{path}/{filename}.npy')
+
+# compress
+csr = csr_matrix(npy)
+
+# save
+mmwrite(f'{path}/{filename}.mtx', csr)
+
+# test
+# a = mmread(f'{path}/{filename}.mtx')
+# print(a)
---
+++ converter/npy2npz.py
@@ -0,0 +1,27 @@
+import argparse
+import numpy as np
+import os
+
+from scipy.sparse import csr_matrix, save_npz, load_npz
+
+# help
+parser = argparse.ArgumentParser(description='.npy to .npz(csr) converter')
+parser.add_argument("path", help="data file path")
+
+# parse argv
+args = parser.parse_args()
+path, basename = os.path.split(args.path)
+filename, ext = os.path.splitext(basename)
+
+# load npy
+npy = np.load(f'{path}/{filename}.npy')
+
+# compress
+csr = csr_matrix(npy)
+
+# save
+save_npz(f'{path}/{filename}.npz', csr)
+
+# test
+# a = load_npz(f'{path}/{filename}.npz')
+# print(a)
