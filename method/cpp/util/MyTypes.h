137e10f9a82aa0ec5efce09440c2e898044c6e0b
takayasumorishita <c011729632@edu.teu.ac.jp>
2021-06-23 18:56:47 +0900

    Move cpp files

--- method/cpp/util/MyTypes.h
+++
@@ -1,9 +0,0 @@
-#include<vector>
-
-namespace MyTypes {
-  using Matrix = std::vector<std::vector<double>>;
-	using vv = std::vector<std::vector<double>>;
-	using ui = unsigned int;
-	using ul = unsigned long;
-	using ll = long long;
-}
