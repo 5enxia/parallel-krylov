50ab41166abc90e3d7a5074369a9edfe54d3f16c
takayasumorishita <c011729632@edu.teu.ac.jp>
2021-09-16 11:48:39 +0900

    Arrange pcg.py

--- x/pipelinecg.py
+++ x/pipelinecg.py
@@ -1,18 +1,16 @@
 import sys
-import os
 import numpy as np
 from numpy import dot
 from numpy.linalg import norm, multi_dot
 
 if __name__ == "__main__":
-    homedir = os.path.expanduser("~")
-    directory = os.path.join(homedir, 'krylov')
-    sys.path.append(directory)
+    sys.path.append('../../../../')
 
-from krylov.method.threads.common import start, end, init
+from krylov.method.single.common import start, end 
+from krylov.method.single.cpu.common import init 
 
 def pcg(A, b, M, epsilon, callback = None, T = np.float64):
-    x, b_norm, N, max_iter, residual, solution_updates = init(A, b, T, pu='cpu')
+    x, b_norm, N, max_iter, residual, solution_updates = init(A, b, T)
     
     start_time = start(method_name='Preconditioned CG')
 
@@ -45,22 +43,21 @@ def pcg(A, b, M, epsilon, callback = None, T = np.float64):
     num_of_iter = i + 1
     residual_index = num_of_iter
 
-    # end(start_time, isConverged, num_of_iter, residual, residual_index)
-    end(start_time, isConverged, num_of_iter, residual[residual_index])
+    end(start_time, isConverged, num_of_iter, residual, residual_index)
     
     return isConverged
 
 
 if __name__ == "__main__":
     import unittest
-    from krylov.util import toepliz_matrix_generator, precondition
+    from krylov.util import loader,toepliz_matrix_generator,precondition
 
     class TestCgMethod(unittest.TestCase):
         epsilon = 1e-4
         T = np.float64
 
         def test_single_pcg_method(self):
-            A, b = toepliz_matrix_generator.generate(N = 100, diag=2.5, sub_diag=1)
+            A, b = toepliz_matrix_generator.generate(N = 2, diag=2.5)
             M = precondition.ilu(A)
             self.assertTrue(pcg(A, b, M ,TestCgMethod.epsilon, TestCgMethod.T))
 
