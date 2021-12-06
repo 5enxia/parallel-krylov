aa7efc5a60cc38615505948bfd3a2b1567171300
takayasumorishita <c011729632@edu.teu.ac.jp>
2021-12-06 20:56:23 +0900

    Add test code

--- test.py
+++ test.py
@@ -7,10 +7,23 @@ elements = np.zeros(N, T)
 elements[0] = 2
 elements[1] = 0.01 
 
-from refactor.cpu.cg import cg
-A, b = toeplitz(elements), np.ones(N, T)
-cg(A, b, 1e-10, T)
+def normal():
+	from refactor.cpu.cg import cg
+	A, b = toeplitz(elements), np.ones(N, T)
+	cg(A, b, 1e-10, T)
 
-# from refactor.cpu.mpi.cg import cg
-# A, b = toeplitz(elements), np.ones(N, T)
-# cg(A, b, 1e-10, T)
+	from refactor.cpu.mrr import mrr
+	A, b = toeplitz(elements), np.ones(N, T)
+	mrr(A, b, 1e-10, T)
+
+def mpi():
+	# from refactor.cpu.mpi.cg import cg
+	# A, b = toeplitz(elements), np.ones(N, T)
+	# cg(A, b, 1e-10, T)
+
+	from refactor.cpu.mpi.mrr import mrr
+	A, b = toeplitz(elements), np.ones(N, T)
+	mrr(A, b, 1e-10, T)
+
+# normal()
+mpi()
