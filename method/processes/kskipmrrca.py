e994f06fea86e2b5e450c59eff939f9d971c91e0
takayasumorishita <c011729632@edu.teu.ac.jp>
2021-12-17 11:19:35 +0900

    Rollback

--- method/processes/kskipmrrca.py
+++ method/processes/kskipmrrca.py
@@ -141,12 +141,9 @@ def _kskipmrr_gpu(A, b, epsilon, k, T, pu):
 
     # mpi recv
     Ax = cp.empty(N, T)
-    local_Ax = cp.empty(local_N, T)
 
     # 初期残差
-    local_Ax = A[begin:end].dot(x)
-    comm.Allgather(local_Ax, Ax)
-    # comm.Allgather(A[begin:end].dot(x), Ax)
+    comm.Allgather(A[begin:end].dot(x), Ax)
     Ar[0] = b - Ax
     residual[0] = norm(Ar[0]) / b_norm
 
@@ -154,9 +151,7 @@ def _kskipmrr_gpu(A, b, epsilon, k, T, pu):
     if rank == 0:
         start_time = start(method_name=f'k-skip MrR + {pu} + mpi + cuda_aware', k=k)
 
-    local_Ax = A[begin:end].dot(Ar[0])
-    comm.Allgather(local_Ax, Ar[1])
-    # comm.Allgather(A[begin:end].dot(Ar[0]), Ar[1])
+    comm.Allgather(A[begin:end].dot(Ar[0]), Ar[1])
     rAr = dot(Ar[0], Ar[1])
     ArAr = dot(Ar[1], Ar[1])
     zeta = rAr / ArAr
@@ -179,13 +174,9 @@ def _kskipmrr_gpu(A, b, epsilon, k, T, pu):
 
         # 基底計算
         for j in range(1, k + 2):
-            local_Ax = A[begin:end].dot(Ar[j-1])
-            comm.Allgather(local_Ax, Ar[1])
-            # comm.Allgather(A[begin:end].dot(Ar[j-1]), Ar[j])
+            comm.Allgather(A[begin:end].dot(Ar[j-1]), Ar[j])
         for j in range(1, k + 1):
-            local_Ax = A[begin:end].dot(Ay[j-1])
-            comm.Allgather(local_Ax, Ay[1])
-            # comm.Allgather(A[begin:end].dot(Ay[j-1]), Ay[j])
+            comm.Allgather(A[begin:end].dot(Ay[j-1]), Ay[j])
 
         # 係数計算
         for j in range(2 * k + 3):
@@ -226,11 +217,7 @@ def _kskipmrr_gpu(A, b, epsilon, k, T, pu):
             d = alpha[2] * delta[0] - beta[1] ** 2
             zeta = alpha[1] * delta[0] / d
             eta = -alpha[1] * beta[1] / d
-            
-            local_Ax = A[begin:end].dot(Ar[0])
-            comm.Allgather(local_Ax, Ar[1])
-            # comm.Allgather(A[begin:end].dot(Ar[0]), Ar[1])
-
+            comm.Allgather(A[begin:end].dot(Ar[0]), Ar[1])
             Ay[0] = eta * Ay[0] + zeta * Ar[1]
             z = eta * z - zeta * Ar[0]
             Ar[0] -= Ay[0]
