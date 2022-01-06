e70d2fe8826ff9ac72ac3f7f258bac0ba2e3949f
takayasumorishita <c011729632@edu.teu.ac.jp>
2022-01-06 20:42:44 +0900

    Add deviceEnablePeerAccess

--- v2/gpu/test.py
+++ v2/gpu/test.py
@@ -15,13 +15,15 @@ for i in range(4):
 	pool = cp.cuda.MemoryPool(cp.cuda.malloc_managed)
 	cp.cuda.set_allocator(pool.malloc)
 
+	cp.cuda.runtime.deviceEnablePeerAccess(i)
+
 	A_list[i] = cp.ones(shape, T)
 	# b_list[i] = cp.arange(N*i, N*(i+1), dtype=T)
 	y_list[i] = cp.zeros(N, T)
 
-# for i in range(4):
-# 	for j in range(0, 4):
-# 		cp.cuda.runtime.deviceCanAccessPeer(i, j)
+for i in range(4):
+	for j in range(0, 4):
+		cp.cuda.runtime.deviceCanAccessPeer(i, j)
 
 for i in range(4):
 	Device(i).use()
