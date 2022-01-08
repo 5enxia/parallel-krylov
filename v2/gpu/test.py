f4f36f49624153fa4f3c10374eca612c91139928
5enxia <c011729632@edu.teu.ac.jp>
2022-01-08 18:19:44 +0900

    Update P2P test code

--- v2/gpu/test.py
+++ v2/gpu/test.py
@@ -2,25 +2,42 @@ import numpy as np
 import cupy as cp
 from cupy.cuda import Device
 
-N = 4
+gpus = 4
+N = gpus * 2
+local_N = N // gpus
 T = np.float64
-shape = (N, N)
+# shape = (N, N)
+shape = (local_N, N)
+
 A_list = [None] * 4
 # b_list = [None] * 4
 b = cp.arange(N, dtype=T)
 y_list = [None] * 4
 
+def gen(i):
+	arr = []
+	for j in range(local_N):
+		begin = (i*local_N+j)*N
+		end = (i*local_N+j+1)*N
+		arr.append(np.arange(begin, end, dtype=np.float64))
+	return arr
+
+
 for i in range(4):
-	ls = [j for j in range(4)]
-	ls.remove(i)
 	Device(i).use()
 	pool = cp.cuda.MemoryPool(cp.cuda.malloc_managed)
 	cp.cuda.set_allocator(pool.malloc)
 
-	for k in ls:
-		cp.cuda.runtime.deviceEnablePeerAccess(k)
+	for j in range(gpus):
+		if i == j:
+			continue
+		cp.cuda.runtime.deviceEnablePeerAccess(j)
 
-	A_list[i] = cp.ones(shape, T)
+	# A_list[i] = cp.ones(shape, T)
+	# A_list[i] = cp.ones(N, T)
+
+	A_list[i] = cp.array(gen(i), T)
+
 	# b_list[i] = cp.arange(N*i, N*(i+1), dtype=T)
 	y_list[i] = cp.zeros(N, T)
 
@@ -32,6 +49,4 @@ for i in range(4):
 	Device(i).use()
 	y_list[i] = A_list[i].dot(b)
 	Device(i).synchronize()
-	print(y_list[i])
-
-
+	print('A:', A_list[i].device, 'b', b.device, 'y', y_list[i].device, y_list[i])
