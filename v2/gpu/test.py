6c7bbaf9f64cff5c31e25f251194e0b9f3a411a3
5enxia <c011729632@edu.teu.ac.jp>
2022-01-17 18:55:06 +0900

    Add async memcpy

--- v2/gpu/test.py
+++ v2/gpu/test.py
@@ -3,10 +3,8 @@ import cupy as cp
 from cupy.cuda import Device
 from time import perf_counter
 
-print('A: P2P + memcpy, B: only memCpy')
-print('B')
 gpus = 4
-rate = 10601
+rate = 5000
 N = gpus * rate
 local_N = N // gpus
 T = np.float64
@@ -18,6 +16,7 @@ b_list = [None] * gpus
 b = cp.arange(N, dtype=T)
 y_list = [None] * gpus
 
+streams = [None] * gpus
 
 # arr gen
 def gen(i):
@@ -32,10 +31,9 @@ def gen(i):
 # init device
 for i in range(gpus):
 	Device(i).use()
+	streams[i] = cp.cuda.Stream()
 	pool = cp.cuda.MemoryPool(cp.cuda.malloc_managed)
 	cp.cuda.set_allocator(pool.malloc)
-	# props = cp.cuda.runtime.getDeviceProperties(i)
-	# print(props)
 
 
 # P2P
@@ -44,11 +42,11 @@ for i in range(gpus):
 	for j in range(gpus):
 		if i == j:
 			continue
-		# flag = cp.cuda.runtime.deviceCanAccessPeer(i, j)
-		# res = 'Enable' if flag else 'Disable'
+		flag = cp.cuda.runtime.deviceCanAccessPeer(i, j)
+		res = 'Enable' if flag else 'Disable'
 		# print(f'{i} -> {j}: {res}', end='')
-		# cp.cuda.runtime.deviceEnablePeerAccess(j)
-		print(i, j)
+		cp.cuda.runtime.deviceEnablePeerAccess(j)
+		# print(i, j)
 		# print()
 
 
@@ -58,22 +56,22 @@ b = cp.ones(N, dtype=T)
 for i in range(gpus):
 	Device(i).use()
 	A_list[i] = cp.array(gen(i), T)
-	b_list[i] = cp.empty(N, T)
+	b_list[i] = cp.ones(N, T)
 	y_list[i] = cp.zeros(N, T)
 
+dic = {'A': 'sync', 'B': 'async'}
+print(dic['A'])
 # dot
-s = perf_counter()
-for j in range(10000):
-	for i in range(gpus):
-		Device(i).use()
-		cp.cuda.runtime.memcpyPeer(b_list[i].data.ptr, 0, b.data.ptr, i, b.nbytes)
-		# y_list[i] = A_list[i].dot(b)
-		y_list[i] = A_list[i].dot(b_list[i])
-	# result
-	for i in range(gpus):
-		Device(i).synchronize()
-print(perf_counter() - s)
-
-for i in range(gpus):
-	# print('A:', A_list[i].device, 'b', b.device, 'y', y_list[i].device, y_list[i])
-	print(y_list[i])
+for k in range(5):
+	s = perf_counter()
+	for j in range(10000):
+		for i in range(gpus):
+			Device(i).use()
+			cp.cuda.runtime.memcpyPeer(b_list[i].data.ptr, 0, b.data.ptr, i, b.nbytes)
+			# cp.cuda.runtime.memcpyPeerAsync(b_list[i].data.ptr, 0, b.data.ptr, i, b.nbytes, streams[i].ptr)
+			y_list[i] = A_list[i].dot(b_list[i])
+		# result
+		for i in range(gpus):
+			# streams[i].synchronize()
+			Device(i).synchronize()
+	print(perf_counter() - s)
