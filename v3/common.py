3b3574a3de3ff2f671eb41108913446a11a22b0c
takayasumorishita <c011729632@edu.teu.ac.jp>
2022-01-24 22:35:53 +0900

    Remove comment

--- v3/common.py
+++ v3/common.py
@@ -1,11 +1,5 @@
 # 標準出力
 def _start(method_name: str = '', k: int = None) -> None:
-    """[summary]
-
-    Args:
-        method_name (str, optional): [description]. Defaults to ''.
-        k (int, optional): [description]. Defaults to None.
-    """
     print('# ', '='*16, ' INFO ', '='*16, ' #', sep='')
     print(f'Method:\t\t{ method_name }')
     if k != None:
@@ -19,15 +13,6 @@ def _finish(
     final_residual: float,
     final_k: int = None
 ) -> None:
-    """[summary]
-
-    Args:
-        elapsed_time (float): [description]
-        isConverged (bool): [description]
-        num_of_iter (int): [description]
-        final_residual (float): [description]
-        final_k (int, optional): [description]. Defaults to None.
-    """
     print(f'Time:\t\t{ elapsed_time } s')
     status = 'converged' if isConverged else 'diverged'
     print(f'Status:\t\t{ status }')
