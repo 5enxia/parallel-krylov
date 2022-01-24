827dafe7323c5da5962d306cbd13129e8e880000
takayasumorishita <c011729632@edu.teu.ac.jp>
2022-01-25 01:54:53 +0900

    Make mrr-mpi-interface to scipy-like

--- v3/cpu/mpi/mrr.py
+++ v3/cpu/mpi/mrr.py
@@ -1,18 +1,20 @@
 import numpy as np
-from numpy import dot
+from numpy import float64, dot
 from numpy.linalg import norm
 
-from .common import start, finish, init, init_mpi
+from .common import start, finish, init, MultiCpu
 
 
-def mrr(A, b, epsilon, T):
+def mrr(comm, local_A, b, x=None, tol=1e-05, maxiter=None, M=None, callback=None, atol=None) -> tuple:
     # MPI初期化
-    comm, rank, num_of_process = init_mpi()
-
-    # 共通初期化
-    local_A, b, x, b_norm, N, max_iter, residual, num_of_solution_updates = init(A, b, T, rank, num_of_process)
+    rank = comm.Get_rank()
+    MultiCpu.joint_mpi(comm)
 
     # 初期化
+    T = float64
+    x, maxiter, b_norm, N, residual, num_of_solution_updates = init(
+        b, x, maxiter)
+    MultiCpu.alloc(local_A, T)
     Ax = np.zeros(N, T)
     Ar = np.zeros(N, T)
     s = np.zeros(N, T)
@@ -22,14 +24,14 @@ def mrr(A, b, epsilon, T):
     mu = np.zeros(1, T)
 
     # 初期残差
-    comm.Allgather(local_A.dot(x), Ax)
+    MultiCpu.dot(local_A, x, out=Ax)
     r = b - Ax
     residual[0] = norm(r) / b_norm
 
     # 初期反復
     if rank == 0:
         start_time = start(method_name='MrR + MPI')
-    comm.Allgather(local_A.dot(r), Ar)
+    MultiCpu.dot(local_A, r, out=Ar)
     rs = dot(r, Ar)
     ss = dot(Ar, Ar)
     zeta = rs / ss
@@ -42,15 +44,15 @@ def mrr(A, b, epsilon, T):
     num_of_solution_updates[1] = 1
 
     # 反復計算
-    while i < max_iter:
+    while i < maxiter:
         # 収束判定
         residual[i] = norm(r) / b_norm
-        isConverged = residual[i] < epsilon
+        isConverged = residual[i] < tol
         if isConverged:
             break
 
         # 解の更新
-        comm.Allgather(local_A.dot(r), Ar)
+        MultiCpu.dot(local_A, r, out=Ar)
         nu = dot(y, Ar)
         mu = dot(y, y)
         gamma = nu / mu
