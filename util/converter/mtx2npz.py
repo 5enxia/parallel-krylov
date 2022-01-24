e05de911625283270d441e91de262a055d855cf1
takayasumorishita <c011729632@edu.teu.ac.jp>
2022-01-24 18:18:38 +0900

    Update mtx2npz converter

--- util/converter/mtx2npz.py
+++ util/converter/mtx2npz.py
@@ -1,15 +1,25 @@
-import os
 import argparse
 from scipy.io import mmread
-from scipy.sparse import save_npz, csr_matrix
+from scipy.sparse import save_npz, bsr_matrix, csc_matrix, csr_matrix, coo_matrix, dia_matrix, dok_matrix, lil_matrix
 
 parser = argparse.ArgumentParser(description='.mtx to .npz converter')
 parser.add_argument("path", help="data file path")
+parser.add_argument("-f", "--format", default="csr",
+                    help="Matrix Storage Format")
 
 args = parser.parse_args()
-path, basename = os.path.split(args.path)
-filename, ext = os.path.splitext(basename)
+
+methods = {
+	"bsr": bsr_matrix,
+    "csc": csc_matrix,
+    "csr": csr_matrix,
+    "coo": coo_matrix,
+    "dia": dia_matrix,
+    "dok": dok_matrix,
+    "lil": lil_matrix,
+}
 
 mtx = mmread(args.path)
-mtx = csr_matrix(mtx)
-save_npz(f'{path}/{filename}.npz', mtx)
+method = methods[args.format]
+npz = method(mtx)
+save_npz(args.path.replace('.mtx', '.npz'), npz)
